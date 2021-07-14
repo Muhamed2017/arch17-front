d67db962ab84b0e10d1b2f00dca2812720cd1eec
gom3a <mohamed.mido85@azhar.edu.eg>
2021-07-14 06:21:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello Arch17 
         </a>
       </header>
     </div>
