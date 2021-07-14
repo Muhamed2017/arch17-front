61b0d176eff9145046990e1888278d723e4c1888
gom3a <mohamed.mido85@azhar.edu.eg>
2021-07-14 20:09:37 +0200

    committing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Register from './pages/Register.jsx';
 
-function App() {
+const  App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello Arch17, Just do it ..
-        </a>
-      </header>
-    </div>
+    <React.Fragement>
+        <div className="" style={{ 'minHeight': 'auto' }}>
+          <Register />
+        </div>
+    </React.Fragement>
   );
 }
 
