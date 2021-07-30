f351c8a5a50fa5e3fc5d92d21b83090d88650167
gom3a <mohamed.mido85@azhar.edu.eg>
2021-07-30 02:51:01 +0200

    committing

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,23 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    // apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    apiKey: 'AIzaSyCFkvNWsIFfBw2t68FIKvtmzLF-un6i7Oc',
+    authDomain: 'testing-28f3c.firebaseapp.com',
+    databaseURL:"https://testing-28f3c.firebaseio.com",
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID ,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+
+    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_FIREBASE_APP_ID ,
+    // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 });
 
 export const auth= app.auth();
