3bcc7780e93962d31df3d2524fc7686a19564fa0
gom3a <mohamed.mido85@azhar.edu.eg>
2021-07-30 03:08:23 +0200

    committing

--- src/firebase.js
+++ src/firebase.js
@@ -6,11 +6,11 @@ const app = firebase.initializeApp({
     apiKey: 'AIzaSyCFkvNWsIFfBw2t68FIKvtmzLF-un6i7Oc',
     authDomain: 'testing-28f3c.firebaseapp.com',
     databaseURL:"https://testing-28f3c.firebaseio.com",
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    projectId: 'testing-28f3c',
+    storageBucket: 'testing-28f3c.appspot.com',
+    messagingSenderId: '696071352639',
+    appId: '1:696071352639:web:602e27e9a04e15c2f7e38a',
+    measurementId: 'G-SLP0R5NV12'
 
     // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
